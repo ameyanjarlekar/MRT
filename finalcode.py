91a978b015819989bcac199fa6a4c1442242db68
ameyanjarlekar <40833548+ameyanjarlekar@users.noreply.github.com>
2018-12-18 11:12:43 +0530

    Update finalcode.py

--- finalcode.py
+++ finalcode.py
@@ -19,5 +19,20 @@ def test(array):
 		i = i+1
 	return store 
 
+def testvalue(array):
+	# a = np.load("/home/ameya/Desktop/MRT/no_ball_h3.npy")
+	new_model = load_model('/home/ameya/Desktop/MRT/new_model_num_coluoured.h5')
+	b = (new_model.predict(array, batch_size=None, verbose=0, steps=None))
+	i = 0
+	store = 0
+	big = 0
+	while i< len(b):
+		z = big
+		big = max(big,b[i][0])
+		if big > z:
+			store = i
+		i = i+1
+	return big
+
 
 
